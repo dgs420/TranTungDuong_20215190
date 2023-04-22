b048aa88a1da217ed720363df43463bf4b281d12
dgs420 <130202477+dgs420@users.noreply.github.com>
2023-04-22 15:02:51 +0700

    Add files via upload

---
+++ Lab01/two/src/two/FirstDialog.java
@@ -0,0 +1,13 @@
+package two;
+import javax.swing.JOptionPane;
+
+public class FirstDialog {
+
+	public static void main(String[] args) {
+		
+		JOptionPane.showMessageDialog(null,"Hello world, how are you?");
+		System.exit(0);
+
+	}
+
+}
